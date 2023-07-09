1f8c1de768b3f806c694da039822c059af578ad1
Paloma Reynolds <palomap.paula@hotmail.com>
2023-07-09 10:52:29 -0400

    refactor: Footer-style

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,8 @@
 import React from "react";
 import "./Footer.css";
+import logo from "./Logo.png";
+import input from "./Email Input Field.png";
+import links from "./Social Links.png";
 
 
 function Footer(props) {
@@ -12,19 +15,13 @@ function Footer(props) {
 
             <div className="black-footer">
                 <div className="logo-footer">
-                    <img src="#" alt="#"/>
+                    <img src={logo} alt="#"/>
                     <p>Copyright © 2020 Landify UI Kit.</p>
                     <p>All rights reserved</p>
                     <div className="icons-footer">
-                        <img src="#" alt="#"/>
-                        <img src="#" alt="#"/>
-                        <img src="#" alt="#"/>
-                        <img src="#" alt="#"/>
+                        <img src={links} alt="#"/>
+                        
                     </div>
-                </div>
-                <div className="columns">
-
-                    
                 </div>
                 <div className="company-col">
                     <h3>Company</h3>
@@ -36,14 +33,15 @@ function Footer(props) {
                 </div>
                 <div className="support-col">
                     <h3>Support</h3>
-                    <a href="#">About us</a>
-                    <a href="#">Blog</a>
-                    <a href="#">Contact us</a>
-                    <a href="#">Pricing</a>
-                    <a href="#">Testimonials</a>
+                    <a href="#">Help center</a>
+                    <a href="#">Terms of service</a>
+                    <a href="#">Legal</a>
+                    <a href="#">Privacy policy</a>
+                    <a href="#">Status</a>
                 </div>
                 <div className="update-col">
                     <h3>Stay up to date</h3>
+                    <img src={input} alt="#"/>
                
                 </div>
 
